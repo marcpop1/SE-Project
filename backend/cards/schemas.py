91865b7872206e589d1d7ed29f62ffb35d496636
Beni Pintea <b.pintea04@gmail.com>
2024-12-29 17:43:08 +0200

    implemented create card

--- backend/cards/schemas.py
+++ backend/cards/schemas.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
+from utils import snake_to_camel
+
 
 class CardDetailResponse(BaseModel):
     id: int
@@ -13,3 +15,11 @@ class CardDetailResponse(BaseModel):
 
     class Config:
         from_attributes = True
+        alias_generator = snake_to_camel
+
+
+class CreateCardRequest(BaseModel):
+    account_id: int
+    card_type: str
+    card_currency: str
+    is_primary: bool
